e711dfa95daff8b7942cc132ff84023c17da788b
koreaitjjhw <139465868+koreaitjjhw@users.noreply.github.com>
2023-09-15 21:43:35 +0900

    Add files via upload

--- Ch04. 연산자/main.js
+++ Ch04. 연산자/main.js
@@ -117,3 +117,12 @@ let n1 = !true;
 let n2 = !false;
 let n3 = !111;
 console.log(n3);
+
+/*
+삼항 연산자
+ - 조건식 ? 값1 : 값2
+ - 조건식이 true이면 값1을 반환하고, false이면 값2를 반환한다.
+*/
+let number = 11;
+let result2 = number % 2 == 1 ? "홀수입니다." : "짝수입니다.";
+console.log(result2);
